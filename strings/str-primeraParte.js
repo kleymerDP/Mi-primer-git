aa3bdd80a449159788c4fefcecf077a19106a68b
kleymerDP <dpkleymer@gmail.com>
2021-08-12 19:32:22 -0500

    Estoy repasando el tipo de dato 'String' y sus metodos

---
+++ strings/str-primeraParte.js
@@ -0,0 +1,12 @@
+
+let mensaje = "Hello World, I'm Programmer";
+console.log(mensaje);
+
+console.log(`- letra en la posición 3: ${mensaje[3]}`);
+console.log(`- logitud: ${mensaje.length}`);
+
+
+// for(let i = 0; i < mensaje.length; i++) {
+    // console.log(mensaje[i]);
+// }
+
